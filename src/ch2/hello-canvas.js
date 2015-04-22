34a9148cb8de2e20402afb25e2700771ea4e46be
Jon Childress <jon@childr.es>
2015-04-22 18:34:32 -0500

    update previous example to new Webgl() syntax

--- src/ch2/hello-canvas.js
+++ src/ch2/hello-canvas.js
@@ -1,8 +1,13 @@
-var helloCanvas = function () {
-  on3dCanvas("webgl", function(gl) {
+var main = function () {
 
+  webgl = new Webgl();
+  webgl.vertShaderSrc = "void main() {}";
+  webgl.fragShaderSrc = "void main() {}";
+
+  webgl.script = function(gl) {
     gl.clearColor(0.0, 0.0, 1.0, 1.0);
     gl.clear(gl.COLOR_BUFFER_BIT);
+  };
 
-  });
+  webgl.runScript();
 }
