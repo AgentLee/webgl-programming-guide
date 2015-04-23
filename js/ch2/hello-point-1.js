75a60e7938b32dce734fd883f92df068490f9942
Jon Childress <jon@childr.es>
2015-04-23 11:40:58 -0500

    arbitrary re-organization

--- js/ch2/hello-point-1.js
+++ js/ch2/hello-point-1.js
@@ -1,8 +1,8 @@
 var main = function () {
 
   webgl = new Webgl();
-  webgl.vertShaderPath = "src/ch2/hello-point-1.vsh";
-  webgl.fragShaderPath = "src/ch2/hello-point-1.fsh";
+  webgl.vertShaderPath = "glsl/ch2/hello-point-1.vsh";
+  webgl.fragShaderPath = "glsl/ch2/hello-point-1.fsh";
 
   webgl.script = function(gl) {
     gl.clearColor(0.0, 0.0, 0.0, 1.0);
