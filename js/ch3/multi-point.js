5b48ce5bb7f7ae03911697e1250e9fd99f023bbd
Jon Childress <jon@childr.es>
2015-04-26 19:23:18 -0500

    partway through ch 3

---
+++ js/ch3/multi-point.js
@@ -0,0 +1,37 @@
+var main = function () {
+  var webgl = new Webgl();
+
+  webgl.vertShaderPath  = "glsl/ch3/multi-point.vsh";
+  webgl.fragShaderPath  = "glsl/ch3/multi-point.fsh";
+  webgl.attrNames       = ["Position"]
+
+  webgl.script = function(gl) {
+    var verticies = [
+      { x: 0.0,   y: 0.5 },
+      { x: -0.5,  y: -0.5 },
+      { x: 0.5,   y: -0.5 }
+    ]
+
+    gl.bindBuffer(gl.ARRAY_BUFFER, gl.createBuffer());
+    gl.bufferData(gl.ARRAY_BUFFER, toGlVertArray(verticies), gl.STATIC_DRAW);
+    gl.vertexAttribPointer(gl.attr.Position, 2, gl.FLOAT, false, 0, 0);
+    gl.enableVertexAttribArray(gl.attr.Position);
+
+    gl.clearColor(0.0, 0.0, 0.0, 1.0);
+    gl.clear(gl.COLOR_BUFFER_BIT);
+    gl.drawArrays(gl.POINTS, 0, verticies.length);
+  };
+
+  webgl.runScript();
+};
+
+var toGlVertArray = function (verticies) {
+  var inline = [];
+
+  for (var i in verticies) {
+    inline.push(verticies[i].x);
+    inline.push(verticies[i].y);
+  }
+
+  return new Float32Array(inline);
+};
---
+++ js/ch3/translated-triangle.js
@@ -0,0 +1,41 @@
+var main = function () {
+  var webgl = new Webgl();
+
+  webgl.vertShaderPath  = "glsl/ch3/translated-triangle.vsh";
+  webgl.fragShaderPath  = "glsl/ch3/hello-triangle.fsh";
+  webgl.attrNames       = ["Position"];
+  webgl.uniNames        = ["Translation"];
+
+  webgl.script = function(gl) {
+    var verticies = [
+      { x: 0.0,   y: 0.5  },
+      { x: -0.5,  y: -0.5 },
+      { x: 0.5,   y: -0.5  }
+    ]
+
+    var translations = [0.5, 0.5, 0.0, 0.0]
+    gl.uniform4fv(gl.uni.Translation, translations);
+
+    gl.bindBuffer(gl.ARRAY_BUFFER, gl.createBuffer());
+    gl.bufferData(gl.ARRAY_BUFFER, toGlVertArray(verticies), gl.STATIC_DRAW);
+    gl.vertexAttribPointer(gl.attr.Position, 2, gl.FLOAT, false, 0, 0);
+    gl.enableVertexAttribArray(gl.attr.Position);
+
+    gl.clearColor(0.0, 0.0, 0.0, 1.0);
+    gl.clear(gl.COLOR_BUFFER_BIT);
+    gl.drawArrays(gl.TRIANGLES, 0, verticies.length);
+  };
+
+  webgl.runScript();
+};
+
+var toGlVertArray = function (verticies) {
+  var inline = [];
+
+  for (var i in verticies) {
+    inline.push(verticies[i].x);
+    inline.push(verticies[i].y);
+  }
+
+  return new Float32Array(inline);
+};
